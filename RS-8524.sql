19aa50061d32cef1c96617ab5970f572a337d9ea
Harrison Hemstreet <harrisonhemstreet@gmail.com>
2022-01-18 14:43:06 -0700

    18 January 2022

---
+++ RS-8524.sql
@@ -0,0 +1,884 @@
+use nxdrems;
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312G2", 
+      "Addl Handling - Dim Ground Zone 2", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      15.75, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312E2", 
+      "Addl Handling - Dim Express Zone 2", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      15.75, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312G3", 
+      "Addl Handling - Dim Ground Zone 3&4", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      17.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312E3", 
+      "Addl Handling - Dim Express Zone 3&4", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      17.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312G5", 
+      "Addl Handling - Dim Ground Zone 5&6", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      19.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312E5", 
+      "Addl Handling - Dim Express Zone 5&6", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      19.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312G7", 
+      "Addl Handling - Dim Ground Zone 7+", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      21.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312E7", 
+      "Addl Handling - Dim Express Zone 7+", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      21.50, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685G2", 
+      "Addl Handling - Weight Ground Zone 2", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      25.25, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685E2", 
+      "Addl Handling - Weight Express Zone 2", 
+      now(), 
+      "fixed", 
+      0, 
+      28, 
+      25.25, 
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685G3", 
+      "Addl Handling - Weight Ground Zone 3&4",
+      now(),
+      "fixed",
+      0,
+      28,
+      27.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685E3", 
+      "Addl Handling - Weight Express Zone 3&4",
+      now(),
+      "fixed",
+      0,
+      28,
+      27.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685G5", 
+      "Addl Handling - Weight Ground Zone 5&6",
+      now(),
+      "fixed",
+      0,
+      28,
+      29.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685E5", 
+      "Addl Handling - Weight Express Zone 5&6",
+      now(),
+      "fixed",
+      0,
+      28,
+      29.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685G7", 
+      "Addl Handling - Weight Ground Zone 7+",
+      now(),
+      "fixed",
+      0,
+      28,
+      31.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "685E7", 
+      "Addl Handling - Weight Express Zone 7+",
+      now(),
+      "fixed",
+      0,
+      28,
+      31.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213G2", 
+      "Oversize - Ground Zone 2",
+      now(),
+      "fixed",
+      0,
+      28,
+      110.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213E2", 
+      "Oversize - Express Zone 2",
+      now(),
+      "fixed",
+      0,
+      28,
+      110.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213G3", 
+      "Oversize - Ground Zone 3&4",
+      now(),
+      "fixed",
+      0,
+      28,
+      120.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213E3", 
+      "Oversize - Express Zone 3&4",
+      now(),
+      "fixed",
+      0,
+      28,
+      120.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213G5", 
+      "Oversize - Ground Zone 5&6",
+      now(),
+      "fixed",
+      0,
+      28,
+      135.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213E5", 
+      "Oversize - Express Zone 5&6",
+      now(),
+      "fixed",
+      0,
+      28,
+      135.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213G7", 
+      "Oversize - Ground Zone 7+",
+      now(),
+      "fixed",
+      0,
+      28,
+      145.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213E7", 
+      "Oversize - Express Zone 7+",
+      now(),
+      "fixed",
+      0,
+      28,
+      145.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "269G2", 
+      "Oversize - Ground @ Home Zone 2",
+      now(),
+      "fixed",
+      0,
+      28,
+      135.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "269G3", 
+      "Oversize - Ground @ Home Zone 3&4",
+      now(),
+      "fixed",
+      0,
+      28,
+      120.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "269G5", 
+      "Oversize - Ground @ Home Zone 5&6",
+      now(),
+      "fixed",
+      0,
+      28,
+      160.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "269G7", 
+      "Oversize - Ground @ Home Zone 7+",
+      now(),
+      "fixed",
+      0,
+      28,
+      170.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312id", 
+      "Addl Handling - Dim - Intl",
+      now(),
+      "fixed",
+      0,
+      28,
+      21.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "312iw", 
+      "Addl Handling - Weight - Intl",
+      now(),
+      "fixed",
+      0,
+      28,
+      31.50,
+      0, 0, 1, 0, 1, 0, 0, 0);
+      
+insert into extracharges
+	(
+      id, 
+      description, 
+      moddate, 
+      type, 
+      perValue, 
+      carrier, 
+      defaultcharge, 
+      minimum, 
+      maximum, 
+      quoteable, 
+      excludeFromRDI, 
+      fuelPart, 
+      basePart, 
+      disabled, 
+      isTax
+    )
+    values 
+    (
+      "213i", 
+      "Oversize - International",
+      now(),
+      "fixed",
+      0,
+      28,
+      145.00,
+      0, 0, 1, 0, 1, 0, 0, 0);
