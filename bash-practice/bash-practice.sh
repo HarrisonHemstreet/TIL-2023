ebc9e77484032d86f9b21288016be57981dc438d
Harry <harrisonhemstreet@gmail.com>
2022-09-22 15:43:17 -0600

    22 September 2022

--- bash-practice/bash-practice.sh
+++ bash-practice/bash-practice.sh
@@ -15,12 +15,35 @@ gdePrime=${gdeArray[0]};
 gdeTwo=${gdeArray[1]};
 gdeThree=${gdeArray[2]};
 oldGdes=();
-start=3;
-count=$((${#gdeArray[@]} - 4))
+count=$((${#gdeArray[@]} - 3))
+
 if [[ $gdePrime == "gde" && $gdeTwo == "gde2" && $gdeThree == "gde3" ]]; then
-  arrayLength=${#gdeArray[@]}-1
-  oldGdes=(gdeArray[@]:$start:$count)
-  echo ${gdeArray};
+  # gather up every gde* directory that is not included in prime, two or three so we can delete them
+  oldGdes=(${gdeArray[@]:3:$count});
 fi
 
+# check to see if there are any old directories we need to delete
+if [[ ${#oldGdes} ]]; then
+  echo "not empty";
+  # ... if so delete them here. loop through here and just rm -rf each directory name
+else
+  echo "empty, or not working"
+fi
+
+# now we will go through and rename the directories, and delete any we don't need
+mv "$gdePrime" "gde2";
+mv "$gdeTwo" "gde3";
+rm -rf "$gdeThree";
+
+# rename the unzipped directory that is default named after the airline
+mv "$airlineName" "gde"
+
 exit 0;
+
+# gde1="gde";
+# gde2="gde2";
+# gde3="gde3";
+
+# mv "$gde1" "gdeONE";
+# mv "$gde2" "gdeTWO";
+# mv "$gde3" "gdeTHREE";
