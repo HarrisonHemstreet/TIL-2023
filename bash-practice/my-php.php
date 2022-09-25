dbf650e68eb0c19e24e91c4bbaa5e40acb1a9dc7
Harry <harrisonhemstreet@gmail.com>
2022-09-25 15:06:01 -0600

    25 September 2022

--- bash-practice/my-php.php
+++ bash-practice/my-php.php
@@ -1,5 +1,5 @@
 <?php
 print 'Hello World';
-$airline_name = "AIXdemo";
-$zip = "private://bundles/AIXdemo-22-09-23-135656.zip";
-shell_exec("./2bash-practice2.sh", "$airline_name", "$zip");
+$airline_name = "airline_name";
+$zip = "zip";
+shell_exec("./my-php.sh $airline_name $zip");
---
+++ bash-practice/my-php.sh
@@ -0,0 +1,2 @@
+touch "$1.txt"
+touch "$2.txt"
